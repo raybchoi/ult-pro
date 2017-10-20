2a4db282e579a99b9be86075d812de4cc60d170b
raybchoi <raybchoi@gmail.com>
2017-10-20 16:23:17 -0700

    completion of bootstrap and jquery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,8 @@
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
+//= require jquery
+//= require jquery_ujs
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
