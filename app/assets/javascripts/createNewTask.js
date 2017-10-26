50bd49417b25508fef884d6af992f8218b594520
raybchoi <raybchoi@gmail.com>
2017-10-25 20:56:05 -0700

    updated @mention

--- app/assets/javascripts/createNewTask.js
+++ app/assets/javascripts/createNewTask.js
@@ -1,112 +1,122 @@
 console.log('User.js file loaded');
 $(document).on('turbolinks:load', function(){
-
-
-
-   $(function(){
-
-     var pizza = ["Jacob Song","Isabella","Ethan","Emma","Michael","Olivia","Alexander","Sophia","William","Ava","Joshua","Emily","Daniel","Madison","Jayden","Abigail","Noah","Chloe","你好","你你你", 'jeremy', "가"];
-     console.log('this is pizz',pizza);
-     let names = $.map(pizza,function(value,i) {
-       return {'id':i,'name':value,'email':value+"@email.com"};
-     });
-     console.log('this is all the names ',names);
-
-      let formatedName = $('#task-assignee_id').val().slice(0, -1)
-
-      var at_config = {
-      at: "@",
-      data: names,
-      headerTpl: '<div class="atwho-header">Member List<small>↑&nbsp;↓&nbsp;</small></div>',
-      insertTpl: '${name}',
-      displayTpl: "<li>${name} <small>${email}</small></li>",
-      limit: 200
-      }
-    $('#task-assignee_id').on('change', function() {
-      for ( let i = 0; i < names.length; i++ ) {
-        console.log(names[i].name);
-        if ( formatedName === names[i].name ) {
-          console.log('this is the name and the ID - ', names[i].name, names[i].id)
-        }
-      }
-   });
-   $('#task-assignee_id').atwho(at_config)
- });
-
-
-
-
-
-
-  $('#create-task-date-picker').datepicker({
-    keyboardNavigation: false,
-    forceParse: false,
-    autoclose: true,
-    todayHighlight: true,
-  });
-  $('#task-type').on('change', function() {
-    let currentSelectedBox = $('#task-type').find(":selected").val()
-    // console.log($('#task-type').find(":selected").val())
-    if ( currentSelectedBox === "#my_own_tasks" ) {
-      $("#task-assignee_id").prop('disabled', true);
-      $("#task-assignee_id").val(`${document.cookie.split('id=')[1]}`);
-      // disable and set the value to the new user ID
-    } else if ( currentSelectedBox === "#tasks_others_owe_me" ) {
-      $("#task-assignee_id").prop('disabled', false);
-      $("#task-assignee_id").val('');
-    }
-  })
-
-
-
-  // console.log('EPIOCH TIME,', moment.unix(1508828400).format("MM/DD/YY"));
-
-  $('#form-create-new-task').on('submit', function() {
-    event.preventDefault();
-    // go and get each of the value from the input field. The initial value of each field was populated automatically by using the 'value=' in the input tag of the html
-    let getSelectDropDownID = document.getElementById("task-type")
-    let tableType = getSelectDropDownID.options[getSelectDropDownID.selectedIndex].value;
-    let newTaskInfo = { task:
-      {
-        title: $('#task-title').val(),
-        description: $('#task-description').val(),
-        assignee_id: $('#task-assignee_id').val(),
-        owner_id: `${document.cookie.split('id=')[1]}`,
-        status: 'Open',
-        // due_date: new Date($('#create-task-date-picker').val())
-        due_date: new Date($('#create-task-date-picker').val()).getTime(),
-      },
-    };
-    // console.log(newTaskInfo);
-
+  // initial ajax call
+  function getAllUsers() {
+    let jsonResponse;
     $.ajax({
-      method: 'POST',
-      url: `/tasks`,
-      data: newTaskInfo,
-      success: function(newlyCreatedTaskData) {
-        console.log('DATA returned from createNewTask', newlyCreatedTaskData);
-        console.log('THIS IS THE TABLE TYPE', tableType);
-        $(tableType).bootstrapTable('insertRow', {
-          index: 0,
-          row: {
-            status: newlyCreatedTaskData.status,
-            title: newlyCreatedTaskData.title.slice(0,120).concat('...').link(`http://localhost:3000/tasks/${newlyCreatedTaskData.id}`),
-            description: newlyCreatedTaskData.description.slice(0,240).concat('...'),
-            taskResponsibleOwner: `${newlyCreatedTaskData.assignee_info.first_name} ${newlyCreatedTaskData.assignee_info.last_name}`,
-            createdDate:  moment(newlyCreatedTaskData.created_date).format("MM/DD/YY"),
-            dueDate: moment(newlyCreatedTaskData.due_date).format("MM/DD/YY"),
-            id: newlyCreatedTaskData.id,
+      method: 'get',
+      url: `/users.json`,
+      success: function(res) {
+        jsonResponse = res
+        console.log('all users - ', jsonResponse);
+        var at_config = {
+          at: "@",
+          data: res,
+          headerTpl: '<div class="atwho-header">Member List<small>↑&nbsp;↓&nbsp;</small></div>',
+          insertTpl: "${atwho-at}${first_name} ${last_name} - ${email}",
+          displayTpl: "<li>${first_name} ${last_name}<small> ${email}</small></li> ",
+          limit: 200
+        }
+        $('#task-assignee_id').atwho(at_config).atwho('run')
+        $('#task-assignee_id').on('change', function() {
+          let findByEmail = $('#task-assignee_id').val().split(' ')[3]
+          console.log(findByEmail);
+          for ( let i = 0; i < res.length; i++ ) {
+            if ( findByEmail === res[i].name ) {
+              console.log('this is the name and the ID - ', res[i].email, res[i].id)
+            }
           }
         });
-        $("#task-assignee_id").prop('disabled', false);
-        $('#form-create-new-task')[0].reset();
       },
       error: function(err) {
-        console.log('ERROR during the createNewUser returned data', err);
+        alert('Sorry! We had an error :(')
       }
     });
+    $('#create-task-date-picker').datepicker({
+      keyboardNavigation: false,
+      forceParse: false,
+      autoclose: true,
+      todayHighlight: true,
+    });
+    $('#task-type').on('change', function() {
+      let currentSelectedBox = $('#task-type').find(":selected").val()
+      // console.log($('#task-type').find(":selected").val())
+      if ( currentSelectedBox === "#my_own_tasks" ) {
+        $("#task-assignee_id").prop('disabled', true);
+        let userName;
+        for ( let i = 0; i < jsonResponse.length; i++ ) {
+          if ( parseInt(`${document.cookie.split('id=')[1]}`) === jsonResponse[i].id ) {
+            userName = `@${jsonResponse[i].first_name} ${jsonResponse[i].last_name} - ${jsonResponse[i].email}`
+          }
+        }
+        $("#task-assignee_id").val(userName);
+        // disable and set the value to the new user ID
+      } else if ( currentSelectedBox === "#tasks_others_owe_me" ) {
+        $("#task-assignee_id").prop('disabled', false);
+        $("#task-assignee_id").val('');
+      }
+    })
+    // console.log('EPIOCH TIME,', moment.unix(1508828400).format("MM/DD/YY"));
+    $('#form-create-new-task').on('submit', function() {
+      event.preventDefault();
+      // go and get each of the value from the input field. The initial value of each field was populated automatically by using the 'value=' in the input tag of the html
+      let findByEmail = $('#task-assignee_id').val().split(' ')[3]
+      console.log(findByEmail);
+      let assignee_number;
+      console.log('res', jsonResponse);
+      for ( let i = 0; i < jsonResponse.length; i++ ) {
+        if ( findByEmail === jsonResponse[i].email ) {
+          assignee_number = jsonResponse[i].id
+          console.log('this is the name and the ID - ', jsonResponse[i].email, res[i].id)
+        }
+      }
+      console.log('assignee_number - ', assignee_number);
+      let getSelectDropDownID = document.getElementById("task-type")
+      let tableType = getSelectDropDownID.options[getSelectDropDownID.selectedIndex].value;
+      let newTaskInfo = { task:
+        {
+          title: $('#task-title').val(),
+          description: $('#task-description').val(),
+          assignee_id: assignee_number,
+          owner_id: `${document.cookie.split('id=')[1]}`,
+          status: 'Open',
+          // due_date: new Date($('#create-task-date-picker').val())
+          due_date: new Date($('#create-task-date-picker').val()).getTime(),
+        },
+      };
+      // console.log(newTaskInfo);
 
-  });
-
+      $.ajax({
+        method: 'POST',
+        url: `/tasks`,
+        data: newTaskInfo,
+        success: function(newlyCreatedTaskData) {
+          console.log('DATA returned from createNewTask', newlyCreatedTaskData);
+          console.log('THIS IS THE TABLE TYPE', tableType);
+          $(tableType).bootstrapTable('insertRow', {
+            index: 0,
+            row: {
+              status: newlyCreatedTaskData.status,
+              title: newlyCreatedTaskData.title.slice(0,120).concat('...').link(`http://localhost:3000/tasks/${newlyCreatedTaskData.id}`),
+              description: newlyCreatedTaskData.description.slice(0,240).concat('...'),
+              taskResponsibleOwner: `${newlyCreatedTaskData.assignee_info.first_name} ${newlyCreatedTaskData.assignee_info.last_name}`,
+              createdDate:  moment(newlyCreatedTaskData.created_date).format("MM/DD/YY"),
+              dueDate: moment(newlyCreatedTaskData.due_date).format("MM/DD/YY"),
+              id: newlyCreatedTaskData.id,
+            }
+          });
+          $("#task-assignee_id").prop('disabled', false);
+          $('#form-create-new-task')[0].reset();
+        },
+        error: function(err) {
+          console.log('ERROR during the createNewUser returned data', err);
+        }
+        // end of post ajax
+      });
+      // #form-create-new-task
+    });
+    // end of getAllUsers function
+  };
+  getAllUsers()
   // end of document.ready
 });
