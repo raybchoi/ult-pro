119a8c07a7432703aab4ff77b1e7a8e45fdf7624
raybchoi <raybchoi@gmail.com>
2017-11-01 12:55:50 -0700

    only broad if task is not my own

--- app/assets/javascripts/channels/tasks.js
+++ app/assets/javascripts/channels/tasks.js
@@ -10,40 +10,44 @@ function createTaskChannel() {
         },
         {
         received: function(resData) {
-          console.log('this is data - ', resData);
-          // console.log('This is the createTaskChannel Data - ', resData);
-          // return $('#messages').append(this.renderMessage(data));
-          let taskInfo = resData.task
-          let assignee_info = resData.assignee_info
-          let owner_info = resData.owner_info
-          $('#tasks_assigned_to_me').bootstrapTable('insertRow', {
-            index: 0,
-            row: {
-              status: taskInfo.status,
-              title:`<span class='task-title fake-link'> ${taskInfo.title.slice(0,120).concat('...')}</span>`,
-              description: taskInfo.description.slice(0,240).concat('...'),
-              taskResponsibleOwner: `${owner_info.first_name} ${owner_info.last_name}`,
-              createdDate:  moment(taskInfo.created_date).format("MM/DD/YY"),
-              dueDate: moment(taskInfo.due_date).format("MM/DD/YY"),
-              id: taskInfo.id,
-              editTask: `<span class="glyphicon glyphicon-pencil fake-span"></span>`,
-              deleteTask: `<span class="glyphicon glyphicon-trash fake-span"></span>`,
-              tableName: 'tasksAssignedToMe',
-            }
-          });
-          $.notify({
-            title: '<strong>Check it out!</strong>',
-            message: 'A new task was assigned to you.'
-          },{
-            type: 'success',
-            timer: 1000,
-            placement: {
-              from: "top",
-              align: "right"
-            },
-            delay: 5000,
-            timer: 1000,
-          });
+          if ( resData.assignee_info.id === resData.owner_info.id ) {
+            return
+          } else {
+            console.log('this is data - ', resData);
+            // console.log('This is the createTaskChannel Data - ', resData);
+            // return $('#messages').append(this.renderMessage(data));
+            let taskInfo = resData.task
+            let assignee_info = resData.assignee_info
+            let owner_info = resData.owner_info
+            $('#tasks_assigned_to_me').bootstrapTable('insertRow', {
+              index: 0,
+              row: {
+                status: taskInfo.status,
+                title:`<span class='task-title fake-link'> ${taskInfo.title.slice(0,120).concat('...')}</span>`,
+                description: taskInfo.description.slice(0,240).concat('...'),
+                taskResponsibleOwner: `${owner_info.first_name} ${owner_info.last_name}`,
+                createdDate:  moment(taskInfo.created_date).format("MM/DD/YY"),
+                dueDate: moment(taskInfo.due_date).format("MM/DD/YY"),
+                id: taskInfo.id,
+                editTask: `<span class="glyphicon glyphicon-pencil fake-span"></span>`,
+                deleteTask: `<span class="glyphicon glyphicon-trash fake-span"></span>`,
+                tableName: 'tasksAssignedToMe',
+              }
+            });
+            $.notify({
+              title: '<strong>Check it out!</strong>',
+              message: 'A new task was assigned to you.'
+            },{
+              type: 'success',
+              timer: 1000,
+              placement: {
+                from: "top",
+                align: "right"
+              },
+              delay: 5000,
+              timer: 1000,
+            });
+          }
         },
         renderMessage: function(data) {
     // return "<p> <b>" + data.user + ": </b>" + data.message + "</p>";
